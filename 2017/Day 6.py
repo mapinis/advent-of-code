baf1658347207d677b96a6c654efb7a9dde1d7b2
Mark Apinis <mapinis2019@gmail.com>
2017-12-06 09:06:46 -0500

    Day 6

---
+++ 2017/Day 6.py
@@ -0,0 +1,17 @@
+nums = [list(line.split("\t")) for line in open("Day 6.txt").readlines()]
+nums = [list(int(num) for num in line) for line in nums][0]
+history = [list(nums)]
+steps = 0
+
+while True:
+    steps += 1
+    m = max(nums)
+    i = nums.index(m)
+    nums[i] = 0
+    for j in range(m):
+        nums[(i + 1 + j) % len(nums)] += 1
+    if nums in history:
+        break
+    history.append(list(nums))
+
+print(steps)
